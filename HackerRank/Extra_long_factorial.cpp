ba17d5900f31f152ee26eb867c5ad9c977db3fa9
nuralam <nuralam862@gmail.com>
2019-12-26 15:01:44 +0000

    Big Factorial Java solution

--- HackerRank/Extra_long_factorial.cpp
+++ HackerRank/Extra_long_factorial.cpp
@@ -61,3 +61,34 @@
 //     extraLongFactorials(5);
 //     return 0;
 // }
+
+// java solution biginteger
+// import java.io.*;
+// import java.math.*;
+// import java.security.*;
+// import java.text.*;
+// import java.util.*;
+// import java.util.concurrent.*;
+// import java.util.regex.*;
+
+// public class Solution {
+
+//     // Complete the extraLongFactorials function below.
+//     static void extraLongFactorials(int n) {
+//         BigInteger fact = BigInteger.ONE;
+//         for(int i = 2; i <= n; i++)
+//             fact = fact.multiply(BigInteger.valueOf(i));
+//         System.out.println(fact);
+//     }
+
+//     private static final Scanner scanner = new Scanner(System.in);
+
+//     public static void main(String[] args) {
+//         int n = scanner.nextInt();
+//         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+//         extraLongFactorials(n);
+
+//         scanner.close();
+//     }
+// }
